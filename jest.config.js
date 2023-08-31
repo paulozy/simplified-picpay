de8a4571a1a87ddff5d89632438bf821a62c6107
Paulo Abreu <paulo.santana@luby.software>
2023-08-31 10:49:46 -0300

    chore: add c

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  modulePaths: ['<rootDir>'],
+  testRegex: '((\\.|/)(spec))\\.(jsx?|tsx?)$',
+  watchPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/',
+    '<rootDir>/coverage/',
+    '<rootDir>/tmp/',
+    '<rootDir>/.docker/',
+  ],
+};
