77aaf71547a850602933043790499d8635ea32e4
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-07 14:55:45 -0300

    feat: add input type to exists method on interface

--- src/@core/domain/repositories/user-repository.interface.ts
+++ src/@core/domain/repositories/user-repository.interface.ts
@@ -1,7 +1,12 @@
 import { User } from "../entities/user";
 
+export type UserExistsInput = {
+  email?: string
+  document?: string
+}
+
 export abstract class UserRepository {
-  abstract exists(email: string): Promise<boolean>
+  abstract exists(input: UserExistsInput): Promise<boolean>
   abstract save(user: User): Promise<void>
   abstract findByEmail(email: string): Promise<User>
   abstract findById(id: string): Promise<User>
