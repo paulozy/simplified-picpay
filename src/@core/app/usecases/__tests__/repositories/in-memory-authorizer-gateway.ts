67f36cc173ff61c1b3a7e5fcd0356203f0c17008
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-12 13:47:12 -0300

    refactor: update authorizer gateway in memory

--- src/@core/app/usecases/__tests__/repositories/in-memory-authorizer-gateway.ts
+++ src/@core/app/usecases/__tests__/repositories/in-memory-authorizer-gateway.ts
@@ -2,8 +2,7 @@ import { AuthorizeResponse, AuthorizerGateway, AuthorizerStatus } from "@core/do
 
 export class InMemoryAuthorizerGateway implements AuthorizerGateway {
   async authorize(): Promise<AuthorizeResponse> {
-    const response = await fetch('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6')
-    const data = await response.json();
+    const data = { message: 'Autorizado' }
 
     if(data.message === 'Não autorizado') return { message: AuthorizerStatus.UNAUTHORIZED }
     
