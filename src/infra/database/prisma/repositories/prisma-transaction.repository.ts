9ad5592446dc3f0c38aa57cda0121c2ae1abedb0
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-12 23:53:39 -0300

    feat: trat: create prisma transaction repository implementation

---
+++ src/infra/database/prisma/repositories/prisma-transaction.repository.ts
@@ -0,0 +1,49 @@
+import { Transaction } from "@core/domain/entities/transaction";
+import { ListTransactionsInput, TransactionRepository } from "@core/domain/repositories/transaction-repository.interface";
+import { Injectable } from "@nestjs/common";
+import { TransactionMapper } from "../mappers/transaction.mapper";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class PrismaTransactionRepository implements TransactionRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async save(transaction: Transaction): Promise<void> {
+    const rawData = TransactionMapper.toPersistance(transaction);
+
+    await this.prisma.transaction.create({
+      data: rawData
+    })
+  }
+  
+  async findById(id: string): Promise<Transaction> {
+    const rawData = await this.prisma.transaction.findUnique({
+      where: {
+        id
+      }
+    })
+
+    const transaction = TransactionMapper.toDomain(rawData);
+
+    return transaction;
+  }
+  
+  async list({payer, payee, date, value}: ListTransactionsInput): Promise<Transaction[]> {
+    const normalizedDate = new Date(date).getTime()
+
+    const rawData = await this.prisma.transaction.findMany({
+      where: {
+        OR: [
+          { payer },
+          { payee }
+        ],
+        created_at: normalizedDate,
+        value
+      }
+    })
+
+    const transactions = rawData.map(rawTransaction => TransactionMapper.toDomain(rawTransaction));
+
+    return transactions;
+  }
+}
