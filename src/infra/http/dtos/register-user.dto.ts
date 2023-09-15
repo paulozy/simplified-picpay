4443da4afa1ef06909b679dda3ead913a87eddf7
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-15 00:00:14 -0300

    refactor: add constraints on register user dto

--- src/infra/http/dtos/register-user.dto.ts
+++ src/infra/http/dtos/register-user.dto.ts
@@ -1,19 +1,30 @@
-import { IsEmail, IsOptional, IsString, Matches } from "class-validator";
+import { IsEmail, IsEnum, IsOptional, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class RegisterUserRules {
   @IsString()
+  @MinLength(3, {
+    message: 'Name must be at least 3 characters long',
+  })
+  @MaxLength(255, {
+    message: 'Name must be a maximum of 255 characters long',
+  })
   name: string;
 
   @IsString()
   @IsEmail()
   email: string;
 
-  @Matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/)
+  @Matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, {
+    message: 'document must be a cpf. Like "000.000.000-00"'
+  })
   // @Matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/)
   document: string;
 
   @IsString()
   @IsOptional()
+  @IsEnum(['common', 'shopkeeper'], {
+    message: 'type must be "common" or "shopkeeper"'
+  })
   type?: string;
 
   constructor(props: RegisterUserRules) {
