4dd3fa46b12a5ae015b444a98193789c0c598701
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-15 00:00:21 -0300

    refactor: add constraints on update user dto

--- src/infra/http/dtos/update-user.dto.ts
+++ src/infra/http/dtos/update-user.dto.ts
@@ -1,12 +1,21 @@
-import { IsOptional, IsString } from "class-validator";
+import { IsEnum, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UpdateUserRules {
   @IsString()
+  @MinLength(3, {
+    message: 'Name must be at least 3 characters long',
+  })
+  @MaxLength(255, {
+    message: 'Name must be a maximum of 255 characters long',
+  })
   @IsOptional()
   name?: string;
 
   @IsString()
   @IsOptional()
+  @IsEnum(['common', 'shopkeeper'], {
+    message: 'type must be "common" or "shopkeeper"'
+  })
   type?: string;
 
   constructor(props: UpdateUserRules) {
