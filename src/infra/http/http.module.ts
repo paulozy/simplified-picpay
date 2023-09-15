a2771b79557044260d118e4d46aac4fb92fc195a
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-14 22:18:00 -0300

    feat: add user controller on http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,10 +1,14 @@
 import { TransactionUseCases } from "@core/app/factories/transaction-usecases.factory";
 import { UserUseCases } from "@core/app/factories/user-usecases.factory";
 import { Module } from "@nestjs/common";
+import { UserController } from "./controllers/user.controller";
 import { TransactionService } from "./services/transaction.service";
 import { UserService } from "./services/user.service";
 
 @Module({
+  controllers: [
+    UserController
+  ],
   providers: [
     UserService,
     UserUseCases,
