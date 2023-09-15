538d6b2c2705d9966b769bbc5c75ff78f1f2fe8e
Paulo Abreu <paulo.abreu@enacom.com.br>
2023-09-14 22:18:16 -0300

    feat: add modules to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './infra/database/database.module';
+import { GatewayModule } from './infra/gateways/gateway.module';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GatewayModule,
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
